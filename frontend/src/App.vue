ba13db390406e8c6df848d95cf7ad7aa9ff69f75
PatrickLAF2k <patrickleonardoaf@gmail.com>
2026-03-15 22:10:37 -0300

    Adicionado a lógica para exibir o SideMenu condicionalmente com base na propriedade hideSidebar da rota.

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,17 +1,18 @@
 <script setup lang="ts">
+import { useRoute } from 'vue-router';
 import SideMenu from './components/SideMenu.vue'
+
+const route = useRoute();
 
 </script>
 
 <template>
   <main class="main">
+    <SideMenu v-if="!route.meta.hideSidebar" />
 
-    <SideMenu />
-
-    <div>
+    <div :class="['content', { 'full-width': route.meta.hideSidebar }]">
       <router-view />
     </div>
-
   </main>
 </template>
 
