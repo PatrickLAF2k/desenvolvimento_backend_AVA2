a995016331a81b2424563a8a8894a157bccde58d
PatrickLAF2k <patrickleonardoaf@gmail.com>
2026-03-15 22:09:16 -0300

    Atualizado o cadastro de usuário para incluir novos campos e garantir que a senha seja obrigatória.

--- backend/src/controllers/usuarios/cadastrarUsuario.ts
+++ backend/src/controllers/usuarios/cadastrarUsuario.ts
@@ -1,5 +1,4 @@
 import { Request, Response } from "express";
-import { v4 as uuidv4 } from "uuid";
 import bcrypt from "bcrypt";
 
 import { usuario } from "../../types";
@@ -8,28 +7,42 @@ import UsuarioModel from "../../models/UsuarioModel";
 
 export async function cadastro(req: Request<{}, {}, usuario>, res: Response) {
     try {
-        const { nome, email, senha } = req.body;
-        const id = uuidv4()
+        const { nome, email, senha, telefone, cep, rua, bairro, cidade, uf } = req.body;
+
+        console.log(req.body);
+
 
         const usuarioExistente = await UsuarioModel.findOne({ where: { email } });
 
         if (usuarioExistente) {
             return res.status(400).json({ message: "Email já cadastrado." });
         }
+        if (!senha) {
+            return res.status(400).json({ message: "A senha é obrigatória." });
+        }
 
         const senhaHash = bcrypt.hashSync(senha, 10);
 
-        UsuarioModel.create({
-            id,
+        await UsuarioModel.create({
             nome,
             email,
             senha: senhaHash,
-            tipo: "paciente"
+            tipo: "admin",
+            telefone,
+            cep,
+            rua,
+            bairro,
+            cidade,
+            uf
         })
 
         res.status(201).json({ message: `Usuário ${nome} cadastrado com sucesso!` })
 
-    } catch (error) {
-        res.status(500).json({ message: "Erro ao cadastrar usuário." })
+    } catch (error: any) {
+
+        res.status(500).json({
+            message: "Erro ao cadastrar usuário.",
+            error: error.message 
+        });
     }
 }
