571b7085508898acd70223376d31659a5dbad38c
PatrickLAF2k <patrickleonardoaf@gmail.com>
2026-03-15 22:11:39 -0300

    Atualizado o roteamento para redirecionar para a página inicial e adicionado as rotas para HomePage e RegistroPage com configuração de meta.

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,23 +1,37 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import AdminDashboard from '../pages/AdminDashboard.vue'
 import AdminConsultas from '../pages/AdminConsultas.vue'
+import HomePage from '../pages/HomePage.vue'
+import RegistroPage from '../pages/RegistroPage.vue'
 
 const routes = [
     {
         path: '/',
-        redirect: '/dashboard'
+        redirect: '/home'
+    },
+    {
+        path: '/home',
+        name: 'HomePage',
+        component: HomePage,
+        meta: { hideSidebar: true }
+    },
+    {
+        path: '/registro',
+        name: 'Registro',
+        component: RegistroPage,
+        meta: { hideSidebar: true }
     },
     {
         path: '/dashboard',
         name: 'Dashboard',
-        component: AdminDashboard
+        component: AdminDashboard,
+        meta: { hideSidebar: false }
     },
     {
         path: '/consultas',
         name: 'Consultas',
         component: AdminConsultas
     },
-
 ]
 
 const router = createRouter({
