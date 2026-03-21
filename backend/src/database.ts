a5c2fc4ad47dfc73eddeb6fc35f4558ff2b52f8c
PatrickLAF2k <patrickleonardoaf@gmail.com>
2026-03-20 21:07:01 -0300

    Adiciona suporte ao PostgreSQL e configura a conexão no Sequelize

--- backend/src/database.ts
+++ backend/src/database.ts
@@ -3,12 +3,26 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-export const sequelize = new Sequelize(
-  process.env.DB_NAME as string,
-  process.env.DB_USER as string,
-  process.env.DB_PASS as string,
-  {
-    host: process.env.DB_HOST,
-    dialect: "mysql"
-  }
-);
+
+const dbUrl = process.env.DATABASE_URL;
+
+export const sequelize = dbUrl 
+  ? new Sequelize(dbUrl, {
+      dialect: "postgres",
+      logging: false,
+      dialectOptions: {
+        ssl: {
+          require: true,
+          rejectUnauthorized: false 
+        }
+      }
+    })
+  : new Sequelize(
+      process.env.DB_NAME as string,
+      process.env.DB_USER as string,
+      process.env.DB_PASS as string,
+      {
+        host: process.env.DB_HOST,
+        dialect: "mysql", 
+      }
+    );
